b862ecaf8fa9d579603cd9f8346364d10dd9b373
Sergio Gabriel <sergio.gabriel.el10@gmail.com>
2022-08-14 13:48:41 -0400

    factory the function

--- index.js
+++ index.js
@@ -8,7 +8,21 @@ const circle={
         console.log('draw')
     }
 };
+//factory function 
+function createCircle(radius,x,y){
+    return {
+        radius,
+        location: {
+            x,y
+        },
+        draw: function(){
+            console.log('Draw')
+        }
+    }
+}
 
+const circle1= createCircle(1,1,1);
 circle.draw();
+circle1.draw();
 
 
