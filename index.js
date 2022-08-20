4be6f99a9c58dec7bfe7a2801dfe672181f9429b
Sergio Gabriel <sergio.gabriel.el10@gmail.com>
2022-08-20 11:06:07 -0400

    private properties

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 //constructor function 
 function Circle(radius){
     this.radius = radius;
+    this.defaultLocation= {x:0, y:0};
+    this.computeOptimumLocation= function(factory){
+    }
     this.draw = function (){
+        this.computeOptimumLocation(0.1);
         console.log('draw');
     } 
 }
 
 const circle = new Circle (1);
-circle.location = {x: 1};
+circle.draw();
