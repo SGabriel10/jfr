0a19dd383ec21f12716a0efe6ad33a0dc8543601
Sergio Gabriel <sergio.gabriel.el10@gmail.com>
2022-08-20 10:52:17 -0400

    adding properties

--- index.js
+++ index.js
@@ -1,51 +1,10 @@
-const circle={
-    radius: 1,
-    location: {
-        x:1,
-        y:1
-    },
-    draw: function() {
-        console.log('draw')
-    }
-};
-//factory function 
-function createCircle(radius,x,y){
-    return {
-        radius,
-        location: {
-            x,y
-        },
-        draw: function(){
-            console.log('Draw')
-        }
-    }
-}
 //constructor function 
 function Circle(radius){
     this.radius = radius;
     this.draw = function (){
-        console.log('draw constructor');
+        console.log('draw');
     } 
 }
-/*
-const circle1= createCircle(1,1,1);
-const another = new Circle(5);
-console.log(another);
-circle.draw();
-circle1.draw();
-another.draw();
-*/
-/*
-let x = {value: 10};
-let y = x;
 
-x.value=20;*/
-
-let obj ={value: 10};
-
-function inscrease(obj){
-    obj.value++;
-}
-
-inscrease(obj);
-console.log(obj);
+const circle = new Circle (1);
+circle.location = {x: 1};
